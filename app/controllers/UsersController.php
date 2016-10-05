3b133daf6f647ebddbe3a564664749357e238f18
Yamilah Atallah <yami@yamilah.com>
2016-10-05 12:50:16 -0400

    Cleaned up routes.php, utilizing user controller.

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+class UsersController extends BaseController {
+	public function index() {
+			$users = User::all();
+			return View::make('users/index', ['users' => $users]);
+	}
+
+	public function show($username) {
+		$user = User::whereUsername($username)->first();
+		return View::make('users/show', ['user' => $user]);
+	}
+}
--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,4 @@
 <?php
 
-Route::get('/', function () {
-	$users = User::all();
-	return View::make('users/index', ['users' => $users]);
-});
-
-Route::get('users/{username}', function($username) {
-	$user = User::whereUsername($username)->first();
-	return View::make('users/show', ['user' => $user]);
-});
+Route::get('users', 'UsersController@index');
+Route::get('users/{username}', 'UsersController@show');
