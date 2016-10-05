f08dec3f5c9bd69d806c7047917d3e4857293041
Yamilah Atallah <yami@yamilah.com>
2016-10-05 10:25:22 -0400

    Added DB config.

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@home');
-Route::get('/about', 'PagesController@about');
-
-Route::get('users', function() {
-	$users = User::find(1);
-	return $user->email;
+Route::get('/', function () {
+	$users = DB::table('users')->get();
+	return $users;
 });
