8994394986eb61449e08edc3158b46a331a863e8
Yamilah Atallah <yami@yamilah.com>
2016-10-05 13:51:15 -0400

    Added function to create new user.

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -3,19 +3,24 @@
 class UsersController extends BaseController {
 	public function index() {
 			$users = User::all();
-			return View::make('users/index', ['users' => $users]);
+			return View::make('users.index', ['users' => $users]);
 	}
 
 	public function show($username) {
 		$user = User::whereUsername($username)->first();
-		return View::make('users/show', ['user' => $user]);
+		return View::make('users.show', ['user' => $user]);
 	}
 
 	public function create() {
-		return View::make('users/create');
+		return View::make('users.create');
 	}
 
 	public function store() {
-		return 'create the new user, given the post data';
+		$user = new User;
+		$user->username = Input::get('username');
+		$user->password = Hash::make(Input::get('password'));
+		$user->save();
+
+		return Redirect::route('users.index');
 	}
 }
